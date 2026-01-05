6651ef12b7ce0b7ed31fb4ce617270225a528073
yunsuper <yunsuper80@gmail.com>
2026-01-05 22:41:33 +0900

    feat: add code

---
+++ index.js
@@ -0,0 +1,5 @@
+export { checkedArr } from "./src/checkedArr.js";
+export { findOddNums } from "./src/findOddNums.js";
+export { findEvenNums } from "./src/findEvenNums.js";
+export { capitalize } from "./src/capitalize.js";
+
